df6c9b7e9bff4408b63533f9baf30e85759ce3b2
Jacob Sherin <jacob@protoship.io>
2022-10-07 16:43:05 +0530

    Converts `Link` enum to an `Option` type alias

--- src/second.rs
+++ src/second.rs
@@ -4,10 +4,7 @@ pub struct List {
     head: Link,
 }
 
-enum Link {
-    Empty,
-    More(Box<Node>),
-}
+type Link = Option<Box<Node>>;
 
 struct Node {
     elem: i32,
@@ -16,22 +13,22 @@ struct Node {
 
 impl List {
     pub fn new() -> Self {
-        List { head: Link::Empty }
+        List { head: None }
     }
 
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, Link::Empty),
+            next: mem::replace(&mut self.head, None),
         });
 
-        self.head = Link::More(new_node);
+        self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, Link::Empty) {
-            Link::Empty => None,
-            Link::More(node) => {
+        match mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
                 self.head = node.next;
                 Some(node.elem)
             }
@@ -41,11 +38,11 @@ impl List {
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut curr_link = mem::replace(&mut self.head, Link::Empty);
-        while let Link::More(mut boxed_node) = curr_link {
-            curr_link = mem::replace(&mut boxed_node.next, Link::Empty)
+        let mut curr_link = mem::replace(&mut self.head, None);
+        while let Some(mut boxed_node) = curr_link {
+            curr_link = mem::replace(&mut boxed_node.next, None)
             // boxed_node goes out of scope and gets dropped here;
-            // but it's Node's `next` field has been set to Link::Empty
+            // but it's Node's `next` field has been set to `None`
             // so no unbounded recursion occurs.
         }
     }
