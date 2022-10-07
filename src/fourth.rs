fe9760aee8702202f0f682e37e01ecf5ae93c3f0
Jacob Sherin <jacob@protoship.io>
2022-10-07 22:57:43 +0530

    Push element onto the back of the deque

--- src/fourth.rs
+++ src/fourth.rs
@@ -56,6 +56,22 @@ impl<T> List<T> {
         }
     }
 
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                // empty list, need to set the head
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+    }
+
     pub fn pop_front(&mut self) -> Option<T> {
         // need to take the old head, ensuring it's -2
         self.head.take().map(|old_head| {
